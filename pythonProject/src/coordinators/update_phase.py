5dfb34adfc9853531e77ea4191608bf6ba8c0594
benz0id <bentudorprice@gmail.com>
2024-06-18 10:58:17 -0400

    whoops

--- pythonProject/src/coordinators/update_phase.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class UpdatePhase:
-    """
-    Responsible for performing updates to managers and neurons after the action
-    phase to prepare for next phase.
-    """
-
--- pythonProject/src/patterns/observer.py
+++
@@ -1,9 +0,0 @@
-from abc import abstractmethod, ABC
-from typing import Any
-
-
-class Observer(ABC):
-
-    @abstractmethod
-    def notify(self, cmd: str, obj: Any):
-        pass
